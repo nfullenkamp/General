d57dad61fb2c42e10d499040408567ae1197b2e4
Nick Fullenkamp <nfullenkamp@gmail.com>
2023-02-18 22:49:14 -0500

    Added Get-MP3MetaData.ps1

---
+++ Get-MP3MetaData.ps1
@@ -0,0 +1,49 @@
+# LINK
+# https://geekeefy.wordpress.com/2016/10/15/powershell-get-mp3mp4-files-metadata-and-how-to-use-it-to-make-you-life-easy/
+
+Function Get-MP3MetaData
+{
+    [CmdletBinding()]
+    [Alias()]
+    [OutputType([Psobject])]
+    Param
+    (
+        [String] [Parameter(Mandatory=$true, ValueFromPipeline=$true)] $Directory
+    )
+
+    Begin
+    {
+        $shell = New-Object -ComObject "Shell.Application"
+    }
+    Process
+    {
+
+        Foreach($Dir in $Directory)
+        {
+            $ObjDir = $shell.NameSpace($Dir)
+            $Files = gci $Dir| ?{$_.Extension -in '.mp3','.mp4'}
+
+            Foreach($File in $Files)
+            {
+                $ObjFile = $ObjDir.parsename($File.Name)
+                $MetaData = @{}
+                $MP3 = ($ObjDir.Items()|?{$_.path -like "*.mp3" -or $_.path -like "*.mp4"})
+                $PropertArray = 0,1,2,12,13,14,15,16,17,18,19,20,21,22,27,28,36,220,223
+
+                Foreach($item in $PropertArray)
+                {
+                    If($ObjDir.GetDetailsOf($ObjFile, $item)) #To avoid empty values
+                    {
+                        $MetaData[$($ObjDir.GetDetailsOf($MP3,$item))] = $ObjDir.GetDetailsOf($ObjFile, $item)
+                    }
+
+                }
+
+                New-Object psobject -Property $MetaData |select *, @{n="Directory";e={$Dir}}, @{n="Fullname";e={Join-Path $Dir $File.Name -Resolve}}, @{n="Extension";e={$File.Extension}}
+            }
+        }
+    }
+    End
+    {
+    }
+}
